7a0fae87299365b4b8837f1a08815dd9cca094c3
Vethika <vethika1681@gmail.com>
2025-02-24 22:42:47 +0530

    AF commit of Day-12

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.send("<h1> Backend</h1>")
+})
+
+const PORT =3001 
+
+app.listen(PORT,()=>console.log("Server Started Successfully"))
+
+
