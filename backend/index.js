ddf9e8a4913b53d4099be58de6df2e4b3e4d778d
Vethika <vethika1681@gmail.com>
2025-02-25 14:44:07 +0530

    AF Commit of Day 13

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mdb = require("mongoose");
 const dotenv = require('dotenv');
 const signup=require('./models/signupSchema')
+const bcrypt = require('bcrypt')
 const app = express();
 app.use(express.json())
 const PORT = 3001;
@@ -26,20 +27,30 @@ app.get("/static", (req, res) => {
   res.sendFile("D:\MERN_2025\Day-4\Task\index.html");
 });
 
-app.post("/signup",(req,res) =>{
+app.post("/signup",async (req,res) =>{
     try{
 
     const {firstName,lastName,email,password,phoneNumber}=req.body
+    const hashedPassword = await bcrypt.hash(password,10)
     const newSignup= new signup({
+      /*async function checkPassword(plainPassword, hashedPassword) {
+    const isMatch = await bcrypt.compare(plainPassword, hashedPassword);
+    if (isMatch) {
+        console.log("✅ Password is correct!");
+    } else {
+        console.log("❌ Password is incorrect!");
+    }
+}
+*/
         firstName: firstName,
         lastName:lastName,
         email:email,
-        password:password,
+        password:hashedPassword,
         phoneNumber:phoneNumber,
     });
     newSignup.save();
     console.log("Signup successfully");
-        res.status(201).json({ message: "Signup Successfully", isSignUp: true });
+        ({ message: "Signup Successfully", isSignUp: true });
     } catch (error) {
     console.log(error);
         res.status(400).json({ message: "Signup Unsuccessfully", isSignUp: false});
