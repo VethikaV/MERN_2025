b840a76b02c767067f5034efdf9bec2faa1d0e57
Vethika <vethika1681@gmail.com>
2025-02-25 13:04:13 +0530

    FN commit of Day-13

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,51 @@
+const express = require("express");
+const mdb = require("mongoose");
+const dotenv = require('dotenv');
+const signup=require('./models/signupSchema')
+const app = express();
+app.use(express.json())
+const PORT = 3001;
+dotenv.config()
 
-const express = require('express')
 
-const app = express()
+mdb
+  .connect("process.env.MONGODB_URL")
+  .then(() => {
+    console.log("MDB Connection Successful");// if it doesnot work connect with local server
+  })
+  .catch((err) => {
+    console.log("Check your connection String", err);
+  });
 
-app.get("/",(req,res)=>{
-    res.send("<h1> Backend</h1>")
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Backend</h1>");
+});
 
-const PORT =3001 
 
-app.listen(PORT,()=>console.log("Server Started Successfully"))
+app.get("/static", (req, res) => {
+  res.sendFile("D:\MERN_2025\Day-4\Task\index.html");
+});
+
+app.post("/signup",(req,res) =>{
+    try{
+
+    const {firstName,lastName,email,password,phoneNumber}=req.body
+    const newSignup= new signup({
+        firstName: firstName,
+        lastName:lastName,
+        email:email,
+        password:password,
+        phoneNumber:phoneNumber,
+    });
+    newSignup.save();
+    console.log("Signup successfully");
+        ({ message: "Signup Successfully", isSignUp: true });
+    } catch (error) {
+    console.log(error);
+        res.status(400).json({ message: "Signup Unsuccessfully", isSignUp: false});
+    }
+});
+
+app.listen(PORT, () => {
+    console.log("Server Started Successfully")
+});
